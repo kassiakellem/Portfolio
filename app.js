cd00042a1636935dcfc253e7d6d3ea78cccf97a1
Kassiakellem <kassiakellem4@gmail.com>
2023-06-27 01:37:14 -0300

    pagina inicial

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// const nome = prompt ("qual seu nome?")
+document.querySelector('.linha-hamburguer-menu').addEventListener('click', function(evento){
+    var menu = document.querySelector('.nav-list');
+    if (menu.style.display === 'flex') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'flex';
+    }
+    var checkbox = document.getElementById('checkbox-menu');
+    checkbox.checked = !checkbox.checked
+        evento.preventDefault();
+  });
+// 
+window.addEventListener("resize", function(){
+  var largura = window.innerWidth
+  var menu = document.querySelector('.nav-list');
+  var checkbox = document.getElementById('checkbox-menu');
+  if(largura>768){
+    menu.style.display = 'flex';
+  }
+  else if(!checkbox.checked) {
+    menu.style.display = 'none';
+  }
+  
+})
